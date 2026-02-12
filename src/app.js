adae8c5ca3514040ed5d60599859e2d540026e78
Ahmet Eren Ceylan <100166488+CarpeDiem0110@users.noreply.github.com>
2026-02-12 16:18:57 +0300

    new package is added

--- src/app.js
+++ src/app.js
@@ -6,5 +6,13 @@ const app = express();
 app.use(express.json());
 app.use("/tasks", taskRoutes);
 
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Internal server error" });
+});
 
 module.exports = app;
